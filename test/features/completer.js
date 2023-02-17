dbd1b2b315ef438b0a0f68a35c86713d1036052e
andy bunce <bunce.andy@gmail.com>
2023-02-17 23:26:04 +0000

    [mod] rework function library

--- test/features/completer.js
+++ test/features/completer.js
@@ -3,10 +3,11 @@ var fs = require('fs');
 var StaticContext = require('../../lib/compiler/static_context').StaticContext;
 
 var XQLint = require('../../lib/xqlint').XQLint;
-var source = 'import module namespace ns="http://basex.org/modules/util";ns:';
+var lib = require('../../lib/xqlint').library;
+var source = 'import module namespace ns="http://basex.org/modules/util";fn:';
 var sctx = new StaticContext(undefined, undefined, 'basex');
-var index = JSON.parse(fs.readFileSync('test/index.json', 'utf-8'));
-sctx.availableModuleNamespaces = Object.keys(index);
+//sctx.setModulesFromXQDoc(lib);
+//sctx.availableModuleNamespaces = Object.keys(lib);
 var linter = new XQLint(source, { staticContext: sctx });
 var pos = { line: 0, character: source.length };
 var proposals = linter.getCompletions(pos);
