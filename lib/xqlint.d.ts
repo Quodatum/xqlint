4b294c9760168de6db7356e38db77493b6af51a9
Andy Bunce <bunce.andy@gmail.com>
2023-10-04 22:57:57 +0100

    [mod] xq-catalog 0.1.0

--- lib/xqlint.d.ts
+++ lib/xqlint.d.ts
@@ -1,69 +1,89 @@
 
 declare module '@quodatum/xqlint' {
-    import { Position } from "vscode";
-    export class XQLint {
-      constructor(source: string, opts?: object);
-      public getCompletions(pos: Position): [object];
-      public getXQDoc(withPos?: boolean): XQDoc;
-      public getAST(pos?: Position): any;
-      public printAST(): string;
-      public getSctx(pos?: Position): any;
-      public getErrors(): [Marker];
-      public getWarnings(): [Marker];
-      public getDocLinks(): [[DocLink]];
-      public getProcessor():string;
-      public getProcessors():[string];
-      public getLibrary():[object];
-      public resolver1(uri:string,ats:[string]);
-    }
-    // 
-    export class Marker {
-      pos: LintRange;
-      type: string; // error,warning
-      level: string; //same as type??
-      message: string; // '[code] ...'
-    }
-    export class DocLink {
-      range: LintRange;
-      uri: string; // uri after at    
-    }
-    export class LintRange {
-      sl: number;
-      sc: number;
-      el: number;
-      ec: number;
-    }
-    export class XQDoc {
-      moduleNamespace: string;
-      description: string;
-      variables: [VarDecl];
-      functions: [FunDecl];
-      queryBody?: LintRange;
-    }
-    export interface VarDecl {
-      name: string;
-      type: string;
-      description?: string;
-      //comments: Comment;
-      pos: LintRange;
-    }
-  
-    export interface FunDecl {
-      name: string;
-      arity: number;
-      params: string[]; // name
-      description?: string;
-      pos: LintRange;
-    }
-    export interface Comment {
-      description: string;
-      params: object;
-      errors: string[];
-      others: string[];
-    }
-    export function XQueryLexer(): any;
-    export function createStaticContext(processor: string,fileName?: string): any;
-    export function CodeFormatter(ast: object): any;
-    export function CodeFormatter(ast: object, newLinesEnabled: boolean, DEBUG: any): any;
-  
-  } 
+  import { Position } from "vscode";
+  export class XQLint {
+    constructor(source: string, opts?: XQLintOptions);
+    public getCompletions(pos: Position): object[];
+    public getXQDoc(withPos?: boolean): XQDoc;
+    public getAST(pos?: Position): Ast;
+    public printAST(indent?: string): string; // XML string
+    public getSctx(pos?: Position): any;
+
+    public getMarkers(): Marker[];
+    public getErrors(): Marker[];
+    public getWarnings(): Marker[];
+
+    public getDocLinks(): DocLink[];
+
+    public getProcessor(): string;
+    public getLibrary(): object[];
+
+    public hasSyntaxError(): boolean;
+    public resolver1(uri: string, ats: string[]): object[];
+  }
+  export function profiles(): any;
+  export function XQueryLexer(): any;
+  export function createStaticContext(processor: string, fileName?: string): any;
+  export function CodeFormatter(ast: object): any;
+  export function CodeFormatter(ast: object, newLinesEnabled: boolean, DEBUG: any): any;
+  export class XQLintOptions {
+    processor: string;
+    fileName?: string; // full path
+    styleCheck?: boolean;
+  }
+  // 
+  export class Marker {
+    pos: LintRange;
+    type: string; // error,warning
+    level: string; //same as type??
+    message: string; // '[code] ...'
+  }
+  export class DocLink {
+    range: LintRange;
+    uri: string; // uri after at    
+  }
+  export class LintRange {
+    sl: number;
+    sc: number;
+    el: number;
+    ec: number;
+  }
+
+  export class XQDoc {
+    moduleNamespace: string;
+    description: string;
+    variables: VarDecl[];
+    functions: FunDecl[];
+    queryBody?: LintRange;
+  }
+  export interface Ast {
+    name: string;
+    children: Ast[];
+    getParent: Ast | null;
+    pos: LintRange;
+    value?: string;
+    [propName: string]: any;
+  }
+
+  export interface VarDecl {
+    name: string;
+    type: string;
+    description?: string;
+    //comments: Comment;
+    pos: LintRange;
+  }
+
+  export interface FunDecl {
+    name: string;
+    arity: number;
+    params: string[]; // name
+    description?: string;
+    pos: LintRange;
+  }
+  export interface Comment {
+    description: string;
+    params: object;
+    errors: string[];
+    others: string[];
+  }
+} 
--- test/processor_test.js
+++ test/processor_test.js
@@ -4,28 +4,32 @@ var assert = require('assert');
 var path = require('path');
 var fs = require('fs');
 
-var XQLint = require('../lib/xqlint').XQLint;
+var XQLint = require('../lib/xqlint');
 vows.describe('Test processor functions').addBatch({
  
-    'name': function () { 
+    'basex10': function () { 
         const pi="basex-10";
-        var linter = new XQLint("42",{processor: pi});
+        var linter = new XQLint.XQLint("42",{processor: pi});
         var p = linter.getProcessor();
    
         assert.equal(p, pi,"Set processor");
     },
-    'processors': function () { 
-        const pi="basex-10";
-        var linter = new XQLint("42",{processor: pi});
-        var p = linter.getProcessors();
+    'basex9': function () { 
+        const pi="basex-9";
+        var linter = new XQLint.XQLint("42",{processor: pi});
+        var p = linter.getProcessor();
    
-        assert.equal(p.length, 3,"processor names");
+        assert.equal(p, pi,"Set processor");
     },
+   
     'library': function () { 
         const pi="basex-10";
-        var linter = new XQLint("42",{processor: pi});
-        var p = linter.getLibrary();
-   
+        var linter = new XQLint.XQLint("42",{processor: pi});
+        var p = linter.getLibrary(); 
         assert.equal(typeof p, "object","library");
+    },
+    'processors': function () { 
+        var p = XQLint.profiles();
+        assert.equal(p.length, 3,"processor names");
     }
 }).export(module);
