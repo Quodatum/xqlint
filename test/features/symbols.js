cbb45324116664a9debc7e2baaa8a1f3bc967b62
Andy Bunce <bunce.andy@gmail.com>
2024-08-16 18:00:26 +0100

    [mod] require node 18+

--- test/features/symbols.js
+++ test/features/symbols.js
@@ -1,12 +1,16 @@
-// generate xqdoc
+// generate symbols
 var fs = require('fs');
 
 var XQLint = require('../../lib/xqlint').XQLint;
-//var src="test/queries/rbtree.xq/map.xq";
-var src="test/queries/issues/pic-datefix.xq";
+var src="test/queries/pdfbox/pdfbox3.xqm";
+//var src="test/queries/issues/pic-datefix.xq";
 //var src="test/queries/rbtree.xq/rbtree.xq";
 //src="cases\history.xqm";
-var linter = new XQLint(fs.readFileSync(src, 'utf-8'));
+const opts = {
+    processor: 'basex-10',
+    styleCheck: false
+};
+var linter = new XQLint(fs.readFileSync(src, 'utf-8'),opts);
 // .hasSyntaxError()
 
 var syms=linter.getSymbols();
